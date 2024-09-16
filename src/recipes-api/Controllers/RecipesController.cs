0bf82f4e251d6018307f89dc142931f64bbbc54c
Artur Peixoto Lucas <arturpeixoto71@gmail.com>
2024-09-16 16:45:18 -0300

    feat delete recipe

--- src/recipes-api/Controllers/RecipesController.cs
+++ src/recipes-api/Controllers/RecipesController.cs
@@ -73,6 +73,14 @@ public class RecipesController : ControllerBase
     [HttpDelete("{name}")]
     public IActionResult Delete(string name)
     {
-        throw new NotImplementedException();
+        try 
+        {
+            this._service.DeleteRecipe(name);
+            return NoContent();
+        }
+        catch
+        {
+            return BadRequest();
+        }
     }    
 }
